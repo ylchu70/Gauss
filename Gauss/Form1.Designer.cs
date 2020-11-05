e8b3b19b46f7fc2bb3e7ee3e820002845fd3d327
James Chu <James.Chu@aver.com>
2020-11-05 13:35:37 +0800

    First push

---
+++ Gauss/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Gauss
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_A11 = new System.Windows.Forms.TextBox();
+            this.textBox_A21 = new System.Windows.Forms.TextBox();
+            this.textBox_A31 = new System.Windows.Forms.TextBox();
+            this.textBox_A12 = new System.Windows.Forms.TextBox();
+            this.textBox_A22 = new System.Windows.Forms.TextBox();
+            this.textBox_A32 = new System.Windows.Forms.TextBox();
+            this.textBox_A13 = new System.Windows.Forms.TextBox();
+            this.textBox_A23 = new System.Windows.Forms.TextBox();
+            this.textBox_A33 = new System.Windows.Forms.TextBox();
+            this.textBox_B1 = new System.Windows.Forms.TextBox();
+            this.textBox_B2 = new System.Windows.Forms.TextBox();
+            this.textBox_B3 = new System.Windows.Forms.TextBox();
+            this.textBox_R1 = new System.Windows.Forms.TextBox();
+            this.textBox_R2 = new System.Windows.Forms.TextBox();
+            this.textBox_R3 = new System.Windows.Forms.TextBox();
+            this.button_solve = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_A11
+            // 
+            this.textBox_A11.Location = new System.Drawing.Point(120, 78);
+            this.textBox_A11.Name = "textBox_A11";
+            this.textBox_A11.Size = new System.Drawing.Size(100, 22);
+            this.textBox_A11.TabIndex = 0;
+            // 
+            // textBox_A21
+            // 
+            this.textBox_A21.Location = new System.Drawing.Point(120, 116);
+            this.textBox_A21.Name = "textBox_A21";
+            this.textBox_A21.Size = new System.Drawing.Size(100, 22);
+            this.textBox_A21.TabIndex = 1;
+            // 
+            // textBox_A31
+            // 
+            this.textBox_A31.Location = new System.Drawing.Point(120, 160);
+            this.textBox_A31.Name = "textBox_A31";
+            this.textBox_A31.Size = new System.Drawing.Size(100, 22);
+            this.textBox_A31.TabIndex = 2;
+            // 
+            // textBox_A12
+            // 
+            this.textBox_A12.Location = new System.Drawing.Point(255, 78);
+            this.textBox_A12.Name = "textBox_A12";
+            this.textBox_A12.Size = new System.Drawing.Size(100, 22);
+            this.textBox_A12.TabIndex = 3;
+            // 
+            // textBox_A22
+            // 
+            this.textBox_A22.Location = new System.Drawing.Point(255, 116);
+            this.textBox_A22.Name = "textBox_A22";
+            this.textBox_A22.Size = new System.Drawing.Size(100, 22);
+            this.textBox_A22.TabIndex = 4;
+            // 
+            // textBox_A32
+            // 
+            this.textBox_A32.Location = new System.Drawing.Point(255, 160);
+            this.textBox_A32.Name = "textBox_A32";
+            this.textBox_A32.Size = new System.Drawing.Size(100, 22);
+            this.textBox_A32.TabIndex = 5;
+            // 
+            // textBox_A13
+            // 
+            this.textBox_A13.Location = new System.Drawing.Point(396, 78);
+            this.textBox_A13.Name = "textBox_A13";
+            this.textBox_A13.Size = new System.Drawing.Size(100, 22);
+            this.textBox_A13.TabIndex = 6;
+            // 
+            // textBox_A23
+            // 
+            this.textBox_A23.Location = new System.Drawing.Point(396, 116);
+            this.textBox_A23.Name = "textBox_A23";
+            this.textBox_A23.Size = new System.Drawing.Size(100, 22);
+            this.textBox_A23.TabIndex = 7;
+            // 
+            // textBox_A33
+            // 
+            this.textBox_A33.Location = new System.Drawing.Point(396, 160);
+            this.textBox_A33.Name = "textBox_A33";
+            this.textBox_A33.Size = new System.Drawing.Size(100, 22);
+            this.textBox_A33.TabIndex = 8;
+            // 
+            // textBox_B1
+            // 
+            this.textBox_B1.Location = new System.Drawing.Point(604, 78);
+            this.textBox_B1.Name = "textBox_B1";
+            this.textBox_B1.Size = new System.Drawing.Size(100, 22);
+            this.textBox_B1.TabIndex = 9;
+            // 
+            // textBox_B2
+            // 
+            this.textBox_B2.Location = new System.Drawing.Point(604, 116);
+            this.textBox_B2.Name = "textBox_B2";
+            this.textBox_B2.Size = new System.Drawing.Size(100, 22);
+            this.textBox_B2.TabIndex = 10;
+            // 
+            // textBox_B3
+            // 
+            this.textBox_B3.Location = new System.Drawing.Point(604, 160);
+            this.textBox_B3.Name = "textBox_B3";
+            this.textBox_B3.Size = new System.Drawing.Size(100, 22);
+            this.textBox_B3.TabIndex = 11;
+            // 
+            // textBox_R1
+            // 
+            this.textBox_R1.Location = new System.Drawing.Point(396, 233);
+            this.textBox_R1.Name = "textBox_R1";
+            this.textBox_R1.Size = new System.Drawing.Size(100, 22);
+            this.textBox_R1.TabIndex = 12;
+            // 
+            // textBox_R2
+            // 
+            this.textBox_R2.Location = new System.Drawing.Point(396, 275);
+            this.textBox_R2.Name = "textBox_R2";
+            this.textBox_R2.Size = new System.Drawing.Size(100, 22);
+            this.textBox_R2.TabIndex = 13;
+            // 
+            // textBox_R3
+            // 
+            this.textBox_R3.Location = new System.Drawing.Point(396, 320);
+            this.textBox_R3.Name = "textBox_R3";
+            this.textBox_R3.Size = new System.Drawing.Size(100, 22);
+            this.textBox_R3.TabIndex = 14;
+            // 
+            // button_solve
+            // 
+            this.button_solve.Location = new System.Drawing.Point(120, 231);
+            this.button_solve.Name = "button_solve";
+            this.button_solve.Size = new System.Drawing.Size(75, 23);
+            this.button_solve.TabIndex = 15;
+            this.button_solve.Text = "Solve";
+            this.button_solve.UseVisualStyleBackColor = true;
+            this.button_solve.Click += new System.EventHandler(this.Button_solve_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_solve);
+            this.Controls.Add(this.textBox_R3);
+            this.Controls.Add(this.textBox_R2);
+            this.Controls.Add(this.textBox_R1);
+            this.Controls.Add(this.textBox_B3);
+            this.Controls.Add(this.textBox_B2);
+            this.Controls.Add(this.textBox_B1);
+            this.Controls.Add(this.textBox_A33);
+            this.Controls.Add(this.textBox_A23);
+            this.Controls.Add(this.textBox_A13);
+            this.Controls.Add(this.textBox_A32);
+            this.Controls.Add(this.textBox_A22);
+            this.Controls.Add(this.textBox_A12);
+            this.Controls.Add(this.textBox_A31);
+            this.Controls.Add(this.textBox_A21);
+            this.Controls.Add(this.textBox_A11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_A11;
+        private System.Windows.Forms.TextBox textBox_A21;
+        private System.Windows.Forms.TextBox textBox_A31;
+        private System.Windows.Forms.TextBox textBox_A12;
+        private System.Windows.Forms.TextBox textBox_A22;
+        private System.Windows.Forms.TextBox textBox_A32;
+        private System.Windows.Forms.TextBox textBox_A13;
+        private System.Windows.Forms.TextBox textBox_A23;
+        private System.Windows.Forms.TextBox textBox_A33;
+        private System.Windows.Forms.TextBox textBox_B1;
+        private System.Windows.Forms.TextBox textBox_B2;
+        private System.Windows.Forms.TextBox textBox_B3;
+        private System.Windows.Forms.TextBox textBox_R1;
+        private System.Windows.Forms.TextBox textBox_R2;
+        private System.Windows.Forms.TextBox textBox_R3;
+        private System.Windows.Forms.Button button_solve;
+    }
+}
+
---
+++ Gauss/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gauss
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_solve_Click(object sender, EventArgs e)
+        {
+            double[,] A = new double[3, 3];
+            double[] B = new double[3];
+            double[] R = new double[3];
+
+            A[0, 0] = double.Parse(textBox_A11.Text);
+            A[0, 1] = double.Parse(textBox_A12.Text);
+            A[0, 2] = double.Parse(textBox_A13.Text);
+            A[1, 0] = double.Parse(textBox_A21.Text);
+            A[1, 1] = double.Parse(textBox_A22.Text);
+            A[1, 2] = double.Parse(textBox_A23.Text);
+            A[2, 0] = double.Parse(textBox_A31.Text);
+            A[2, 1] = double.Parse(textBox_A32.Text);
+            A[2, 2] = double.Parse(textBox_A33.Text);
+
+            B[0] = double.Parse(textBox_B1.Text);
+            B[1] = double.Parse(textBox_B2.Text);
+            B[2] = double.Parse(textBox_B3.Text);
+
+            for (int i=1; i<3; i++)
+            {
+                double C = A[i, 0] / A[0, 0];
+                for (int j=0; j<3; j++)
+                {
+                    A[i, j] -= (C * A[0, j]);
+                }
+                B[i] -= (C * B[0]);
+            }
+
+            double D = A[2, 1] / A[1, 1];
+            for (int j=1; j<3; j++)
+            {
+                A[2, j] -= (D * A[1, j]);
+            }
+            B[2] -= (D * B[1]);
+
+            R[2] = B[2] / A[2, 2];
+            R[1] = (B[1] - A[1, 2] * R[2]) / A[1, 1];
+            R[0] = (B[0] - A[0, 2] * R[2] - A[0, 1] * R[1]) / A[0, 0];
+
+            textBox_R1.Text = R[0].ToString();
+            textBox_R2.Text = R[1].ToString();
+            textBox_R3.Text = R[2].ToString();
+        }
+    }
+}
